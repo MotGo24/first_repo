8255100386627c7dc3aa5cef9649c8c2e171cf33
Matvey Gorbachev <mgorbachev.02@gmail.com>
2023-12-18 11:42:53 +0300

    Added

---
+++ DateAndTime/src/Main.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Дата и время");
+        System.out.println(LocalDateTime.now());
+    }
+}
